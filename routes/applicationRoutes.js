d204a133ac9a30bd4f96e6163bf87dda041ba651
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-11-01 09:58:33 +0530

    added

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getSeekerApplications, updateApplication } from '../controllers/applicationController.js';
+import { getApplicationForJob, getSeekerApplications, updateApplication } from '../controllers/applicationController.js';
 import { protect, role } from '../middleware/authMiddleware.js'
 const router = express.Router();
 
 router.get('/' , protect , role ('seeker') , getSeekerApplications);
 router.put('/:id/status' , protect , role('company' , 'admin') , updateApplication);
-
+// Example route to add:
+router.route('/job/:jobId').get(protect, getApplicationForJob);
 export default router;
